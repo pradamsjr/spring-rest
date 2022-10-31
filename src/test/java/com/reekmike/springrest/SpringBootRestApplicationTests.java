f25930ff9dc284e0ea4dc1c4ea2e8d35be7bf47f
Rico Adams <57771033+pradamsjr@users.noreply.github.com>
2022-10-31 16:47:47 -0400

    Initial checkin.

---
+++ src/test/java/com/reekmike/springrest/SpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reekmike.springrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
